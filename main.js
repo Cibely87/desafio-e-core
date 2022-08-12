eeaa9eea8f58e3ba5d84349357b3564d32bfda25
Cibely Pereira <cibely.port@gmail.com>
2022-08-11 21:58:02 -0300

    criação mainMenu e listMenu

---
+++ main.js
@@ -0,0 +1,18 @@
+const mainMenu = `
+======== MAIN MENU ========
+
+a > Add person
+l > List people
+q > Quit
+Type a option:
+`
+
+const listMenu = `
+======== LIST PEOPLE BY... ========
+
+a > Age
+n > Name
+q > Quit
+Type a option:
+`
+
