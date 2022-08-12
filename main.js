9d2024fe7f1747cca0bb86641f781901e1c18cf2
Cibely Pereira <cibely.port@gmail.com>
2022-08-12 12:46:03 -0300

    criação readline

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const readline = require('readline-sync')
+
 const mainMenu = `
 ======== MAIN MENU ========
 
@@ -16,3 +18,59 @@ q > Quit
 Type a option:
 `
 
+const people = []
+
+const getCategory = (age) => {
+    if (age >= 0 && age <= 12) {
+        return "Child"
+    }
+    if (age >= 13 && age <= 19) {
+        return "Teenager"
+    }
+    if (age >= 20 && age <= 64) {
+        return "Adult"
+    }
+
+    return "Old-aged"
+}
+
+console.log(mainMenu)
+readline.promptCLLoop({
+    a: () => {
+        const name = readline.question("Name: ")
+        const age = readline.question("Age: ")
+        const category = getCategory(age)
+        people.push({ name, age, category })
+        console.log(mainMenu)
+    },
+    l: () => {
+        console.log(listMenu)
+        readline.promptCLLoop({
+            a: () => {
+                const sorted = people.sort(
+                    (personA, personB) => personA.age - personB.age
+                )
+                console.table(sorted)
+                console.log(listMenu)
+            },
+            n: () => {
+                const sorted = people.sort(
+                    (a, b) => {
+                        if (a.name > b.name) {
+                            return 1;
+                        }
+                        if (a.name < b.name) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                )
+                console.table(sorted)
+                console.log(listMenu)
+            },
+            q: () => true
+        })
+        console.log(mainMenu)
+    },
+    q: () => true
+})
